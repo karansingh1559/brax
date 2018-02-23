73cbc73ff0a92ce459e185df2384e31b777d245a
Karan Singh <karansingh1559@gmail.com>
2018-02-24 00:14:24 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+//fill in the tokens here of your particular app
+
+module.exports = {
+  consumer_key:         '', 
+  consumer_secret:      '',
+  access_token:         '',
+access_token_secret: ''
+}
